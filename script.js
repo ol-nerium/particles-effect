0927522da468af0d820cc9f763dd38971c85d91c
al-burlachenko <alexandrburl@gmail.com>
2025-11-27 13:52:41 +0200

    impoves bouncing effect+ads gradient

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-ctx.fillStyle = "white";
+console.log("ctx", ctx);
 
-// console.log("ctx", ctx);
+const gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height); // x0 y0 x1 y1
+gradient.addColorStop(0, "white");
+gradient.addColorStop(0.5, "magenta");
+gradient.addColorStop(1, "blue");
+
+ctx.fillStyle = gradient;
+
 // ctx.strokeStyle = "white";
 // ctx.lineWidth = 5;
 // ctx.fillRect(50, 50, 20, 100); //x, y, w, h
@@ -13,18 +19,18 @@ ctx.fillStyle = "white";
 class Particle {
   constructor(effect) {
     this.effect = effect;
-    this.radius = Math.random() * 40 + 5;
+    this.radius = Math.random() * 10 + 5;
     this.x =
       this.radius + Math.random() * (this.effect.width - this.radius * 2);
     this.y =
       this.radius + Math.random() * (this.effect.height - 2 * this.radius);
-    this.vx = 1;
+    this.vx = Math.random() * 1 - 0.5;
+    this.vy = Math.random() * 1 - 0.5;
   }
   draw(context) {
-    // context.fillStyle = "hsl(30, 100%, 50%)";
-    context.fillStyle = `hsl(${this.x * 0.5}, 100%, 50%)`;
-    // context.fillStyle = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    // context.fillStyle = `hsl(${this.x * 0.5}, 100%, 50%)`;
     // hue saturation lightness
+
     context.beginPath();
     context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
     context.fill();
@@ -33,7 +39,12 @@ class Particle {
 
   update() {
     this.x += this.vx;
-    if (this.x > this.effect.width || this.x < 0) this.vx *= -1;
+    if (this.x > this.effect.width - this.radius || this.x < this.radius)
+      this.vx *= -1;
+
+    this.y += this.vy;
+    if (this.y > this.effect.height - this.radius || this.y < this.radius)
+      this.vy *= -1;
   }
 }
 
@@ -43,7 +54,7 @@ class Effect {
     this.width = this.canvas.width;
     this.height = this.canvas.height;
     this.particles = [];
-    this.numberOfParticles = 200;
+    this.numberOfParticles = 52;
     this.createParticles();
   }
 
